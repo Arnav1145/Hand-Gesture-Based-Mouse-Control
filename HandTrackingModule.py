58c9efab818b67a161c56fd3f9121a7773a5ad3b
ARNAV MODANWAL <74757456+Arnav1145@users.noreply.github.com>
2023-02-26 15:24:10 +0530

    Update HandTrackingModule.py

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -4,8 +4,6 @@ import time
 import math
 import numpy as np
 
-a = 3
-
 class handDetector():
     def __init__(self, mode=False, maxHands=2, detectionCon=0.7, trackCon=0.7):
         self.mode = mode
